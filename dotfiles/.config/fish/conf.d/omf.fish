8d548acc3fa3c5a30173169ed48e51334538288c
Harrison Hall <hachha@hachha.dev>
2026-04-26 17:20:13 -0400

    fix: conditionally load omf

--- dotfiles/.config/fish/conf.d/omf.fish
+++ dotfiles/.config/fish/conf.d/omf.fish
@@ -4,4 +4,4 @@ set -q XDG_DATA_HOME
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+type -f $OMF_PATH/init.fish 2&>/dev/null && source $OMF_PATH/init.fish
